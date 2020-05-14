e9a2b314c0a9af95c520f9eab0ede8769b638263
unknown <contact@alexcass.co>
2020-05-14 09:27:01 -0400

    add menu button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./header.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [menuOpen, toggleMenu] = useState(false);
+
   return (
     <header>
+      <div className={`menu menu--spin ${menuOpen ? 'is-active' : ''}`} onClick={() => toggleMenu(!menuOpen)}>
+        <div className="menu-box">
+          <div className="menu-inner"></div>
+        </div>
+      </div>
       <Link to="/">GABA</Link>
       <Link to="/profile">Profile</Link>
     </header>
