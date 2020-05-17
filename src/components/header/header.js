5c56d2840d36e3646b854dfaf844b45df626e762
unknown <contact@alexcass.co>
2020-05-17 07:59:59 -0400

    add fonts and menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [menuOpen, toggleMenu] = useState(false);
+    const [menuOpen, toggleMenu] = useState(false);
 
-  return (
-    <header>
-      <div className={`menu menu--spin ${menuOpen ? 'is-active' : ''}`} onClick={() => toggleMenu(!menuOpen)}>
-        <div className="menu-box">
-          <div className="menu-inner"></div>
-        </div>
-      </div>
-      <Link to="/">GABA</Link>
-      <Link to="/profile">Profile</Link>
-    </header>
-  );
+    return (
+        <>
+            <header>
+                <div
+                    className={`menu menu--spin ${menuOpen ? "is-active" : undefined}`}
+                    onClick={() => toggleMenu(!menuOpen)}
+                >
+                    <div className="menu-box">
+                        <div className="menu-inner"></div>
+                    </div>
+                </div>
+                <Link className="logo-type" to="/">GABA</Link>
+                <Link to="/profile">Profile</Link>
+            </header>
+            <nav className={menuOpen ? "open" : undefined}>
+                <p>menu</p>
+            </nav>
+        </>
+    );
 };
 
 export default Header;
